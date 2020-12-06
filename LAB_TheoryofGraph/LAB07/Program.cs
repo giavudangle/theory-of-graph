6b383ba096fee628cbe3272a978a3b1b8a808fec
chemthan <kms.vudang@gmail.com>
2020-12-07 01:41:34 +0700

    Update LAB07

--- LAB_TheoryofGraph/LAB07/Program.cs
+++ LAB_TheoryofGraph/LAB07/Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/**
+ * LAB07 : Shortest Path Dijsktra - FloyWarshall
+ * Created by Vudang
+ * Published on 28/11/2020
+ */
+
+using System;
 using System.Collections.Generic;
 
 namespace Dijsktra
@@ -6,7 +12,7 @@ namespace Dijsktra
     class Program
     {
         static int n, m, s, t;
-        static LinkedList<(int, int)>[] adj;
+        static LinkedList<Tuple<int, int>>[] adj;
         static int[] pre;
         static int[] dist;
         static bool[] processed;
@@ -16,6 +22,9 @@ namespace Dijsktra
 
         // Add for bài 2
         static int x;
+        static LinkedList<int> pathFromX;
+        static LinkedList<int> pathToY;
+        static int totalCost = 0;
 
         static void ReadData()
         {
@@ -26,86 +35,45 @@ namespace Dijsktra
             s = int.Parse(tokens[2]);
             t = int.Parse(tokens[3]);
 
-            adj = new LinkedList<(int, int)>[n];
+            adj = new LinkedList<Tuple<int, int>>[n + 1];
 
-            for (int i = 0; i< n; i++)
+            for (int i = 1; i <= n; i++)
             {
-                adj[i] = new LinkedList<(int, int)>();
+                adj[i] = new LinkedList<Tuple<int, int>>();
             }
-            for(int i = 0; i < m; i++)
+            for (int i = 1; i <= m; i++)
             {
                 string line = Console.ReadLine();
                 if (line == "") continue;
                 tokens = line.Split(' ');
-                int u = int.Parse(tokens[0]) - 1;
-                int v = int.Parse(tokens[1]) - 1;
+                int u = int.Parse(tokens[0]);
+                int v = int.Parse(tokens[1]);
                 int w = int.Parse(tokens[2]);
-                adj[u].AddLast((v, w));
-                adj[v].AddLast((u, w));
+                adj[u].AddLast(new Tuple<int, int>(v, w));
+                adj[v].AddLast(new Tuple<int, int>(u, w));
             }
         }
 
-        static void Dijkstra()
+        static void Dijkstra(int start)
         {
-            dist = new int[n];
-            pre = new int[n];
-            processed = new bool[n];
+            dist = new int[n + 1];
+            pre = new int[n + 1];
+            processed = new bool[n + 1];
 
-            for(int i = 0; i < n; i++)
+            for (int i = 1; i <= n; i++)
             {
                 dist[i] = INF;
                 pre[i] = -1;
                 processed[i] = false;
             }
-            dist[s] = 0;
-            for(int k = 0; k < n; k++)
+            dist[start] = 0;
+
+            for (int k = 1; k <= n; k++)
             {
                 int a = -1;
-                int min = INF;
-                for(int i = 0; i < n; i++)               
-                    if (processed[i] == false && (a == -1 || dist[a] > dist[i]))
-                        a =i;
-                if (dist[a] == INF) break;
-                processed[a] = true;
-
-                foreach(var edge in adj[a])
-                {
-                    int b = edge.Item1;
-                    int w = edge.Item2;
-                    if(dist[b] > dist[a] + w)
-                    {
-                        dist[b] = dist[a] + w;
-                        pre[b] = a;
-                    }
-                }
-                
-            }
-            
-        }
-
-        static LinkedList<int> root = new LinkedList<int>();
-
-        static void DijsktraMod()
-        {
-            dist = new int[n];
-            pre = new int[n];
-            processed = new bool[n];
-
-            for (int i = 0; i < n; i++)
-            {
-                dist[i] = INF;
-                pre[i] = -1;
-                processed[i] = false;
-            }
-            dist[s] = 0;
-            for (int k = 0 ; k < x - 1; k++) // Config here
-            {
-                int a = -1;
-                int min = INF;
-                for (int i = 0; i < n; i++)
+                for (int i = 1; i <= n; i++)
                     if (processed[i] == false && (a == -1 || dist[a] > dist[i]))
                         a = i;
-
                 if (dist[a] == INF) break;
                 processed[a] = true;
 
@@ -122,75 +90,17 @@ namespace Dijsktra
 
             }
 
-            int e = x - 1;
-            while (pre[e] != -1) // Khi chưa đụng thằng có pre= -1 tức chưa hết
-            {
-                root.AddFirst(pre[e] + 1);
-                e = pre[e];
-            }
-            root.AddFirst(s);
-           
-            // Custom
-
-            for (int i = 0; i < n; i++)
-            {
-                dist[i] = INF;
-                pre[i] = -1;
-                processed[i] = false;
-            }
-            dist[s] = 0;
-            for (int k = x -1 ; k < n; k++) // Config here
-            {
-                int a = -1;
-                int min = INF;
-                for (int i = 0; i < n; i++)
-                    if (processed[i] == false && (a == -1 || dist[a] > dist[i]))
-                        a = i;
-
-                if (dist[a] == INF) break;
-                processed[a] = true;
-
-                foreach (var edge in adj[a])
-                {
-                    int b = edge.Item1;
-                    int w = edge.Item2;
-                    if (dist[b] > dist[a] + w)
-                    {
-                        dist[b] = dist[a] + w;
-                        pre[b] = a;
-                    }
-                }
-
-            }
-
-            foreach(var x in root)
-            {
-                Console.Write(x + " ");
-            }
         }
 
-       
-
-        static void processPreviousPath()
+        static void FindPreviousPath()
         {
             path = new LinkedList<int>();
-            int e = t; // Lấy đỉnh kết thúc 
-            t = t - 1; // Trong danh sách đánh số từ 0 nên phải trừ 1 để đủ size cho Graph
-            path.AddLast(e);
 
-            while (pre[t] != -1) // Khi chưa đụng thằng có pre= -1 tức chưa hết
-            {
-                path.AddFirst(pre[t] + 1); // Add First để đảo chiều
-                t = pre[t];
-            }
-            path.AddFirst(s);
+            for (int i = t; i != -1; i = pre[i])
+                path.AddFirst(i);
 
-            Console.WriteLine(dist[s-1] + dist[e-1]);
-            foreach(var x in path)
-            {
-                Console.Write(x + " ");
-            }
-
+            foreach (int z in path)
+                Console.Write(z + " ");
         }
 
         static void ReadDataGraph()
@@ -203,22 +113,22 @@ namespace Dijsktra
             t = int.Parse(tokens[3]);
             x = int.Parse(tokens[4]);
 
-            adj = new LinkedList<(int, int)>[n];
+            adj = new LinkedList<Tuple<int, int>>[n + 1];
 
-            for (int i = 0; i < n; i++)
+            for (int i = 1; i <= n; i++)
             {
-                adj[i] = new LinkedList<(int, int)>();
+                adj[i] = new LinkedList<Tuple<int, int>>();
             }
-            for (int i = 0; i < m; i++)
+            for (int i = 1; i <= m; i++)
             {
                 string line = Console.ReadLine();
                 if (line == "") continue;
                 tokens = line.Split(' ');
-                int u = int.Parse(tokens[0]) - 1;
-                int v = int.Parse(tokens[1]) - 1;
+                int u = int.Parse(tokens[0]);
+                int v = int.Parse(tokens[1]);
                 int w = int.Parse(tokens[2]);
-                adj[u].AddLast((v, w));
-                adj[v].AddLast((u, w));
+                adj[u].AddLast(new Tuple<int, int>(v, w));
+                adj[v].AddLast(new Tuple<int, int>(u, w));
             }
         }
 
@@ -227,15 +137,124 @@ namespace Dijsktra
         {
             /* Bài 1. Đường đi ngắn nhất */
             //ReadData();
-            //Dijkstra();
-            //processPreviousPath();
+            //Dijkstra(s);
+            //Console.WriteLine(dist[t]);
+            //FindPreviousPath();
 
             /* Bài 2.  Đường đi ngắn nhất qua đỉnh trung gian 
              * Chỉ cần chạy Dijsktra từ s-> x rồi chạy từ x -> t là ra 
              */
-            ReadDataGraph();
-            //DijsktraModStart();
-            DijsktraMod();
+            //ReadDataGraph();
+            ///*Chạy Dijsktra từ S -> X*/
+            //Dijkstra(s);
+            //totalCost = dist[x];
+            //pathFromX = new LinkedList<int>();
+            //for (int i = x; i != -1; i = pre[i])            
+            //    pathFromX.AddFirst(i);            
+            //Dijkstra(x);
+            //totalCost += dist[t];
+            //pathToY = new LinkedList<int>();
+            //for (int i = t; i != -1; i = pre[i])
+            //    pathToY.AddFirst(i);
+            //Console.WriteLine(totalCost);
+            //pathToY.RemoveFirst();
+            //foreach (int item in pathFromX)
+            //    Console.Write(item + " ");
+            //foreach (int item in pathToY)
+            //    Console.Write(item + " ");
+
+            /*Bài 3. Đường đi ngắn nhất giữa các cặp đỉnh*/
+            //FloydWarshall floydWarshall = new FloydWarshall();
+            //floydWarshall.ReadData();
+            //floydWarshall.FloydWarshallFunction();
+
+
+
+
         }
+    }
+
+    class FloydWarshall
+    {
+        public int n { get; set; }
+        public int[,] dist { get; set; }
+        public int[,] a { get; set; }
+        public int[,] pre { get; set; }
+
+        const int INF = (int)1e9;
+
+        public void ReadData()
+        {
+            n = int.Parse(Console.ReadLine());
+            a = new int[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                string[] tokens = Console.ReadLine().Split(' ');
+                for (int j = 0; j < tokens.Length; j++)
+                    a[i, j] = int.Parse(tokens[j]);
+            }
+
+        }
+
+        public void FloydWarshallFunction()
+        {
+            dist = new int[n, n];
+            pre = new int[n, n];
+            // Initialize
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i == j)
+                        dist[i, j] = 0;
+                    if (a[i, j] > 0)
+                    {
+                        dist[i, j] = a[i, j];
+                        pre[i, j] = i;
+                    }
+                    else
+                    {
+                        dist[i, j] = INF;
+                        pre[i, j] = i;
+                    }
+                }
+            }
+
+            // Loop function
+
+
+
+            for (int k = 0; k < n; k++)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        if (dist[i, j] > (dist[i, k] + dist[k, j]))
+                        {
+                            dist[i, j] = dist[i, k] + dist[k, j];
+                            pre[i, j] = pre[i, k];
+                        }
+                    }
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    if (i == j)
+                        dist[i, j] = 0;
+
+
+
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                    Console.Write(dist[i, j] + " ");
+                Console.WriteLine();
+            }
+
+        }
+
     }
 }
